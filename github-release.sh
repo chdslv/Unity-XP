47ce13cf489854b540f1ed1a5757787c7fca2440
Raul Dipeas <rauldipeas@mail.com>
2019-11-02 14:15:10 -0300

    Azure Pipelines

--- github-release.sh
+++ github-release.sh
@@ -1,8 +1,8 @@
 tag="`date '+%d-%m-%y-%H-%M-%S'`"
 wget -cO github-release https://github.com/buildkite/github-release/releases/download/v1.0/github-release-linux-amd64
 chmod +x -v github-release
-./github-release "Azure pipelines auto release" ../unity-xp-19.10-amd64.iso\
- --commit "master"\
- --tag "$tag"\
- --github-repository "rauldipeas/Unity-XP"\
+./github-release "Azure pipelines auto release" $HOME/Unity-XP/unity-xp-19.10-amd64.iso \
+ --commit "master" \
+ --tag "$tag" \
+ --github-repository "rauldipeas/Unity-XP" \
  --github-access-token "$GITHUB_ACCESS_TOKEN"
