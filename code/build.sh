d1c8cfb8e6a7d1df60f45f28d6cd621bd9f87586
Raul Dipeas <rauldipeas@mail.com>
2019-11-06 01:32:11 -0300

    ^build.sh

--- code/build.sh
+++ code/build.sh
@@ -166,13 +166,14 @@ sudo cp -rfv code/settings/nvidia-composite.desktop $HOME/Unity-XP/chroot/etc/xd
 sudo cp -rfv code/settings/99qt5ct.conf $HOME/Unity-XP/chroot/etc/environment.d/99qt5ct.conf
 sudo mkdir -p $HOME/Unity-XP/chroot/etc/skel/.config/{dconf,Kvantum,qt5ct,olivevideoeditor.org/Olive}
 sudo cp -rfv $HOME/Unity-XP/chroot/usr/share/applications/appimaged*.desktop $HOME/Unity-XP/chroot/etc/xdg/autostart/
-#keyboard-indicator(off)
+#Keyboard-indicator(off)
 #Yaru++-Dark(icons)
 #vimix-dark-laptop-ruby(gtk-theme)
 #Breeze(cursor)
 #Wallpaper(ubuntu-glitch-logo)
 #Tilix on Nemo(open terminal here)
 #Tap-to-click(touchpad)
+#Unity launcher(favorites)
 sudo cp -rfv code/settings/user $HOME/Unity-XP/chroot/etc/skel/.config/dconf/user
 sudo cp -rfv code/settings/kvantum.kvconfig $HOME/Unity-XP/chroot/etc/skel/.config/Kvantum/kvantum.kvconfig
 sudo cp -rfv code/settings/config.xml $HOME/Unity-XP/chroot/etc/skel/.config/olivevideoeditor.org/Olive/config.xml
