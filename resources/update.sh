a86cbc6e0f51c7930db2b5f1e3cd6eae418ce4cf
Raul Dipeas <rauldipeas@mail.com>
2019-11-11 06:14:05 -0300

    ^update.sh

--- resources/update.sh
+++ resources/update.sh
@@ -17,6 +17,7 @@ promptyn () {
 if promptyn "Você deseja atualizar o AppImageD? (s/n)"; then
   wget -c https://github.com/rauldipeas/Unity-XP/raw/master/resources/appimaged_1-alpha-git0f1c320.travis214_amd64.deb
   sudo apt install -y ./appimaged_1-alpha-git0f1c320.travis214_amd64.deb;rm appimaged*.deb
+  sudo cp -rfv /usr/share/applications/appimaged*.desktop /etc/xdg/autostart/
 else
   clear
 fi
