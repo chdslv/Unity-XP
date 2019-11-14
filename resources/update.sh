9eff416f2bcafef37e7f8d28dd9f6d881cf4cdcb
Raul Dipeas <rauldipeas@mail.com>
2019-11-14 02:59:51 -0300

    ^update.sh

--- resources/update.sh
+++ resources/update.sh
@@ -132,8 +132,9 @@ promptyn () {
 
 if promptyn "Você deseja mudar os ícones de status para o tema Papirus? (s/n)"; then
   sudo rm -rfv usr/share/icons/Yaru++/status/* /usr/share/icons/Yaru++-Dark/status/*
+  sudo sed -i 's/Papirus-Dark/Papirus/g' /usr/share/icons/Yaru++-Dark/index.theme
   sudo ln -s /usr/share/icons/Papirus/22x22/panel/ /usr/share/icons/Yaru++/status/24
-  sudo ln -s /usr/share/icons/Papirus-Dark/22x22/panel/ /usr/share/icons/Yaru++-Dark/status/24
+  sudo ln -s /usr/share/icons/Papirus/22x22/panel/ /usr/share/icons/Yaru++-Dark/status/24
 else
   clear
 fi
