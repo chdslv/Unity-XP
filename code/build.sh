33ce2461695dca3a8888990d719840ca52a64278
Raul Dipeas <rauldipeas@mail.com>
2019-11-15 01:44:45 -0300

    ^build.sh

--- code/build.sh
+++ code/build.sh
@@ -123,7 +123,7 @@ sudo mkdir -pv $HOME/Unity-XP/chroot/etc/skel/.local/share/applications
 sudo unzip GpuTest_Linux_x64_0.7.0.zip -d $HOME/Unity-XP/chroot/etc/skel/.local/share/
 sudo wget -cO $HOME/Unity-XP/chroot/etc/skel/.local/share/applications/gputest.desktop https://github.com/rauldipeas/Unity-XP/raw/master/resources/gputest.desktop
 sudo chroot $HOME/Unity-XP/chroot sh -c "wget -qO- https://raw.githubusercontent.com/Bonandry/yaru-plus/master/install.sh | sh"
-sudo sed -i 's/Humanity/Papirus,Humanity/g' $HOME/Unity-XP/chroot/usr/share/icons/Yaru++/index.theme
+sudo sed -i 's/Yaru/Papirus/g' $HOME/Unity-XP/chroot/usr/share/icons/Yaru++/index.theme
 sudo rm -rfv $HOME/Unity-XP/chroot/usr/share/icons/Yaru++/status/*
 sudo chroot $HOME/Unity-XP/chroot sh -c "ln -sv /usr/share/icons/Papirus/22x22/panel/ /usr/share/icons/Yaru++/status/24"
 sudo chroot $HOME/Unity-XP/chroot sh -c "git clone https://github.com/vinceliuice/vimix-gtk-themes;cd vimix-gtk-themes;./Install"
